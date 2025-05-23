8e5872ae94fc077eba3b61407fdb38feba3ce5c4
Alexander Mitrokhin <alexamm@stud.ntnu.no>
2025-05-23 12:45:05 +0200

    Updated readme

--- supabase/schemas/[5]_finally_2.sql
+++ supabase/schemas/[5]_finally_2.sql
@@ -37,7 +37,7 @@ VALUES
     'admin@stud.ntnu.no',
     '{"provider":"email","providers":["email"]}',
     '{}',
-    FALSE,
+    TRUE,
     crypt('admin', gen_salt('bf')),
     NOW(),
     NOW(),
@@ -68,6 +68,73 @@ VALUES
     'email',
     (SELECT id FROM auth.users WHERE email = 'admin@stud.ntnu.no'),
     json_build_object('sub', (SELECT id FROM auth.users WHERE email = 'admin@stud.ntnu.no')),
+    NOW(),
+    NOW(),
+    NOW()
+  );
+
+-- user user
+INSERT INTO
+  auth.users (
+    id,
+    instance_id,
+    ROLE,
+    aud,
+    email,
+    raw_app_meta_data,
+    raw_user_meta_data,
+    is_super_admin,
+    encrypted_password,
+    created_at,
+    updated_at,
+    last_sign_in_at,
+    email_confirmed_at,
+    confirmation_sent_at,
+    confirmation_token,
+    recovery_token,
+    email_change_token_new,
+    email_change
+  )
+VALUES
+  (
+    gen_random_uuid(),
+    '00000000-0000-0000-0000-000000000000',
+    'authenticated',
+    'authenticated',
+    'user@stud.ntnu.no',
+    '{"provider":"email","providers":["email"]}',
+    '{}',
+    FALSE,
+    crypt('user', gen_salt('bf')),
+    NOW(),
+    NOW(),
+    NOW(),
+    NOW(),
+    NOW(),
+    '',
+    '',
+    '',
+    ''
+  );
+
+INSERT INTO
+  auth.identities (
+    id,
+    provider_id,
+    provider,
+    user_id,
+    identity_data,
+    last_sign_in_at,
+    created_at,
+    updated_at
+  )
+VALUES
+  (
+    (SELECT id FROM auth.users WHERE email = 'user@stud.ntnu.no'),
+    (SELECT id FROM auth.users WHERE email = 'user@stud.ntnu.no'),
+    'email',
+    (SELECT id FROM auth.users WHERE email = 'user@stud.ntnu.no'),
+    json_build_object('sub', (SELECT id FROM auth.users WHERE email = 'user@stud.ntnu.no')),
     NOW(),
     NOW(),
     NOW()
