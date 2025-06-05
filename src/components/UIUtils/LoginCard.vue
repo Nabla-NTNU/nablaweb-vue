13e189f83c16a5bc19f6f27853bbe4cdb386781e
Jonathan Kjørlaug <jonathan@kjorlaug.no>
2025-06-05 23:02:41 +0200

    Error messages for failed login

--- src/components/UIUtils/LoginCard.vue
+++ src/components/UIUtils/LoginCard.vue
@@ -2,21 +2,29 @@
 
 <script setup>
     import { ref } from 'vue';
-    import { supabase } from '@/lib/supabaseClient'
+    import { useAuth } from '@/composables/useAuth';
 
+    const { signIn} = useAuth()
 
     const username = ref("")
     const password = ref("")
 
-    // Supabase only allows email + password, not username + password We
-    //  therefore add an "@stud.ntnu.no" to avoid any awkward clashes.
+    const errorMessage = ref(null)
+
+    const hideMessage = () => showError.value = null
+
     async function handleLogin() {
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email: username.value + "@stud.ntnu.no",
-            password: password.value
-        })
-        if (error) {
-            console.log(error)
+        try {
+            const _ = await signIn( username.value + "@stud.ntnu.no", password.value)
+        }
+        catch (error) {
+            if (error.name == "AuthApiError") {
+                errorMessage.value = "Vennligst oppgi korrekt brukernavn og passord.";
+            }
+
+            else {
+                console.log(error)
+            }
         }
     }
 </script>
@@ -30,5 +38,16 @@
         <button class="px-4 py-2 rounded-lg text-white font-semibold transition-all duration-300 bg-primary" type="submit" @click="handleLogin">
             Log in
         </button>
+        <br/>
+        <div v-if="errorMessage" class="border-error-300 bg-error-200 p-4 m-4 rounded-xl border-2 flex flex-row">
+            <p class="basis-3/4">
+                {{errorMessage}}
+            </p>
+            <div class="basis-1/4 place-content-center mr-5">
+                <button class="text-error-500 hover:text-error-900 float-right" @click="hideMessage">
+                    x
+                </button>
+            </div>
+        </div>
     </form>
 </template>
