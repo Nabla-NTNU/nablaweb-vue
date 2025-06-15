0afae4c33d24537495b7ac20b9cf56428741d889
Jonathan Kjørlaug <jonathan@kjorlaug.no>
2025-06-15 16:40:01 +0200

    (Hopefully) working useNablaUser!

--- src/composables/useNablaUser.ts
+++ src/composables/useNablaUser.ts
@@ -1,10 +1,7 @@
 import { supabase } from "@/lib/supabaseClient"
 import { QueryData } from "@supabase/supabase-js"
 import { Ref, ref, onMounted } from "vue"
-import { NablaUser, NablaGroup, GroupMember } from "@/lib/types/frontend.types"
-import { useGroups } from "./useNablaGroup"
-
-const { groups } = useGroups()
+import { NablaUser, NablaGroup, GroupKind } from "@/lib/types/frontend.types"
 
 async function getUserData(username: string) {
     try {
@@ -36,9 +33,48 @@ async function getUserData(username: string) {
         const queryResult: userData = data
         return queryResult
     } catch (error) {
-        console.error(`[useNablaGroup] error: failed to get user data:`, error)
+        console.error(`[useNablaUser] error: failed to get user data:`, error)
     }
     return null
+}
+
+async function getMembershipData(username: string) {
+    try {
+        const membershipQuery = supabase
+            .schema("nablaweb_vue")
+            .from("nabla_group_members")
+            .select(
+                `
+                user,
+                group: nabla_groups(
+                    id,
+                    name,
+                    kind,
+                    logo,
+                    mail_list,
+                    leader_mail,
+                    leader,
+                    about,
+                    group_photo,
+                    date_began,
+                    is_active
+                ),
+                is_active
+            `,
+            )
+            .eq("user", username)
+            .eq("is_active", true)
+        type userData = QueryData<typeof membershipQuery>
+        const { data, error } = await membershipQuery
+        if (error) throw error
+        const queryResult: userData = data
+        return queryResult
+    } catch (error) {
+        console.error(
+            `[useNablaUser] error: failed to get group membership data:`,
+            error,
+        )
+    }
 }
 
 export function useUser(username: string) {
@@ -53,7 +89,9 @@ export function useUser(username: string) {
                 username: userData.username,
                 firstName: userData.first_name,
                 lastName: userData.last_name,
-                profilePicture: new URL(userData.profile_picture),
+                profilePicture: userData.profile_picture
+                    ? new URL(userData.profile_picture)
+                    : undefined,
                 isActive: userData.is_active,
                 class: userData.class,
                 memberOf: [],
@@ -64,7 +102,9 @@ export function useUser(username: string) {
                 birthday: userData.birthday
                     ? new Date(userData.birthday)
                     : null,
-                website: new URL(userData.website),
+                website: userData.website
+                    ? new URL(userData.website)
+                    : undefined,
             }
         }
         await updateGroupMembership()
@@ -72,29 +112,38 @@ export function useUser(username: string) {
     })
 
     async function updateGroupMembership() {
-        if (user.value) {
-            let memberships: NablaGroup[] = []
+        isLoading.value = true
+        const memberData = await getMembershipData(username)
 
-            isLoading.value = true
-
-            memberships = groups.value.filter((group: NablaGroup) => {
-                if (group && group.members) {
-                    const memberNames = group.members.map(
-                        (member: GroupMember) => member.user.username,
-                    )
-                    return memberNames.includes(username)
-                } else {
-                    console.error("Group or group.members is undefined")
+        if (user.value && memberData) {
+            if (memberData.length == 0) {
+                user.value.memberOf = []
+            }
+            const groups: NablaGroup[] = memberData?.map((membership) => {
+                const data = membership.group
+                const group: NablaGroup = {
+                    id: data.id,
+                    name: data.name,
+                    kind:
+                        data.kind in GroupKind
+                            ? (data.kind as GroupKind)
+                            : undefined, // Will fail silently for unknown GroupKinds
+                    logo: new URL(data.logo),
+                    mailList: data.mail_list,
+                    leader: { user: { username: data.leader! } },
+                    about: data.about,
+                    groupPhoto: new URL(data.group_photo),
+                    date: new Date(data.date_began),
+                    isActive: data.is_active,
                 }
+                return group
             })
 
-            user.value.memberOf = memberships
-
-            isLoading.value = false
+            user.value.memberOf = groups
         } else {
-            console.error("User is undefined")
-            return false
+            console.error("[NablaUser] User or membership data is not defined")
         }
+        isLoading.value = false
     }
 
     return { user, isLoading, error, updateGroupMembership }
