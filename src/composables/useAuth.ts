87f9301205352cde81454a39611af1d6c589efb2
Alexander Mitrokhin <alexamm@stud.ntnu.no>
2025-06-23 23:40:46 +0200

    cleaned useAuth

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,153 +1,187 @@
 import { ref, computed, readonly } from "vue"
 import { supabase } from "@/lib/supabaseClient"
-import type { User, Session, AuthError } from "@supabase/supabase-js"
+import type {
+    User as SupabaseUser,
+    Session,
+    AuthError,
+} from "@supabase/supabase-js"
+import type { NablaUser } from "@/lib/types/frontend.types"
 
-const user = ref<User | null>(null)
+const supabaseUser = ref<SupabaseUser | null>(null)
 const session = ref<Session | null>(null)
-const username = ref<string | null>(null)
-const profilePicture = ref<URL | null>(null)
-const isInitialised = ref<boolean>(false)
+const nablaUser = ref<NablaUser | null>(null)
+const isAdmin = ref<boolean>(false) // Refactor for granular admin access
+
+// guard to only initialize once
+let initializePromise: Promise<void> | undefined
+
+function resetUser() {
+    supabaseUser.value = null
+    session.value = null
+    nablaUser.value = null
+    isAdmin.value = false
+}
+
+function toURL(url: string): URL | undefined {
+    try {
+        return new URL(url)
+    } catch (error) {
+        console.error(`[useAuth] Erronious URL "${url}"`, error)
+        return undefined
+    }
+}
 
 // In the future: this can be stored as meta-data in Supabase! Probably should too.
 // https://supabase.com/docs/guides/auth/managing-user-data
-async function getUser(): Promise<void> {
-    if (user.value?.id) {
+async function fetchSession() {
+    try {
+        const { data, error } = await supabase.auth.getSession()
+        if (error) throw error
+        session.value = data.session
+
+        if (data.session?.user) {
+            supabaseUser.value = data.session.user
+        } else {
+            resetUser()
+        }
+    } catch (e) {
+        console.error("[useAuth] Could not get session", e)
+    }
+}
+
+async function fetchNablaUser() {
+    if (supabaseUser.value?.id) {
         try {
             const { data, error } = await supabase
                 .schema("nablaweb_vue")
                 .from("nabla_users")
                 .select(
-                    `username, first_name, last_name, profilePicture: profile_picture`,
+                    `
+                        username,
+                        firstName: first_name,
+                        lastName: last_name,
+                        profilePicture: profile_picture
+                    `,
                 )
-                .eq("supabase_id", user.value.id)
+                .eq("supabase_id", supabaseUser.value?.id)
                 .single()
             if (error) throw error
-            username.value = data.username
-            try {
-                profilePicture.value = new URL(data.profilePicture)
-            } catch (error) {
-                console.error(
-                    `[useAuth] user had a non-URL profile picture ${user.value.id}`,
-                    error,
-                )
+            nablaUser.value = {
+                username: data.username,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                profilePicture: toURL(data.profilePicture),
             }
         } catch (error) {
             console.error(
-                `[useAuth] error finding username for user ${user.value.id}`,
+                `[useAuth] Failed to fetch NablaUser with supabase ID ${supabaseUser.value?.id}`,
                 error,
             )
         }
     } else {
-        username.value = null
+        console.error(
+            `[useAuth] Fetching user when not logged in. Resetting auth.`,
+        )
+        resetUser()
     }
 }
 
-// Rewrite for granular admin access issue. Probably fetch user permissions or sumfin
-async function isUserAdmin(): Promise<boolean> {
-    try {
-        if (!username.value) {
-            await getUser()
+// Refactor for granular admin access
+async function fetchUserPermissions() {
+    if (nablaUser.value?.username) {
+        try {
+            const { data, error } = await supabase
+                .schema("nablaweb_vue")
+                .from("nabladmins")
+                .select("user")
+                .eq("user", nablaUser.value?.username)
+            if (error) throw error
+            if (data.length > 0) {
+                isAdmin.value = true
+                return
+            }
+        } catch (error) {
+            console.error(
+                `[useAuth] error checking if user ${nablaUser.value?.username} is admin`,
+                error,
+            )
         }
-
-        const { data, error } = await supabase
-            .schema("nablaweb_vue")
-            .from("nabladmins")
-            .select("user")
-            .eq("user", username.value!)
-
-        if (data) {
-            return data.length > 0
-        }
-        if (error) {
-            throw error
-        }
-    } catch (e) {
+    } else {
         console.error(
-            `[useAuth] error checking if user ${username.value} is admin`,
-            e,
+            `[useAuth] Fetching user permissions of a user not logged in. Resetting auth.`,
         )
     }
-    return false
+    isAdmin.value = false
 }
 
-async function initialize(): Promise<void> {
+async function renewUser() {
+    resetUser()
+    await fetchSession()
+    if (supabaseUser.value?.id) {
+        await fetchNablaUser()
+        await fetchUserPermissions()
+    }
+}
+
+async function initialize() {
     try {
-        const { data, error } = await supabase.auth.getSession()
-        if (error) throw error
-        session.value = data.session
-        user.value = data.session?.user ?? null
-        await getUser()
+        await renewUser()
     } catch (e) {
         console.error("[useAuth] init error", e)
-    } finally {
-        // Make global listener to auth state change
-        const authListener = supabase.auth.onAuthStateChange(
-            (_event, newSession) => {
-                session.value = newSession
-                user.value = newSession?.user ?? null
-                getUser()
-            },
-        )
-        // And clear the memory manually if app is left
-        window.onbeforeunload = () =>
-            authListener.data.subscription.unsubscribe()
-        isInitialised.value = true
     }
 }
-
-// Initialise if havent yet. Must be awaited for guards to work.
-if (!isInitialised.value) await initialize()
 
 async function signIn(
     email: string,
     password: string,
 ): Promise<null | AuthError> {
     try {
-        const { error, data } = await supabase.auth.signInWithPassword({
+        const { error } = await supabase.auth.signInWithPassword({
             email,
             password,
         })
         if (error) throw error
-        user.value = data.user
-        session.value = data.session
-        getUser()
-        return error
+        await renewUser()
+        return null
     } catch (error) {
         console.error("[useAuth] Error signing in", error)
         return error as AuthError
     }
 }
 
-async function signOut() {
+async function signOut(): Promise<null | AuthError> {
     try {
         const { error } = await supabase.auth.signOut()
         if (error) throw error
-        user.value = null
-        session.value = null
-        username.value = null
-        return error
+        await renewUser()
+        // window.location.replace("/")
+        return null
     } catch (error) {
         console.error("[useAuth] Error signing out", error)
+        resetUser()
         return error as AuthError
     }
 }
 
-async function signUp(email: string, password: string) {
-    const { error } = await supabase.auth.signUp({ email, password })
-    if (error) throw error
+// async function signUp(email: string, password: string) {
+//     const { error } = await supabase.auth.signUp({ email, password })
+//     if (error) throw error
+// }
+
+if (!initializePromise) {
+    initializePromise = initialize()
+    await initializePromise
 }
 
 export function useAuth() {
-    const isAuthenticated = computed(() => !!user.value)
-
     return {
-        username: readonly(username),
-        profilePicture: readonly(profilePicture),
-        isAuthenticated,
+        username: computed(() => nablaUser.value?.username),
+        profilePicture: computed(() => nablaUser.value?.profilePicture),
+        isAuthenticated: computed(() => !!supabaseUser.value),
+        isAdmin: readonly(isAdmin),
 
         signIn,
-        signUp,
         signOut,
-        isUserAdmin,
+        // signUp,
     }
 }
