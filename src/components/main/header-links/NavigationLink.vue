ae9bb2c0a00ccc0463b17046a7d133a9d9a4f5c8
Alexander Mitrokhin <alexamm@stud.ntnu.no>
2025-06-23 23:42:18 +0200

    prettier desktop header

--- src/components/main/header-links/NavigationLink.vue
+++ src/components/main/header-links/NavigationLink.vue
@@ -21,15 +21,16 @@
          and not 100% reliable. If anyone wants to
          redo, you're very welcome. Is doable via
          DOM listeners, I'm just not confident in
-         using such things yet.-->
+         using such things yet. -->
         <div
-            class="absolute left-1/2 top-[80%] -z-10 flex origin-top -translate-x-1/2 -translate-y-full flex-col content-center bg-primary pt-4 text-fg opacity-0 transition-all duration-200 group-hover:-translate-y-[0px] group-hover:opacity-100"
+            v-if="dropdownItems"
+            class="absolute left-1/2 top-[80%] -z-10 flex origin-top -translate-x-1/2 -translate-y-full flex-col content-center rounded-b-[30px] bg-primary p-2 pt-4 text-fg opacity-0 transition-all duration-200 group-hover:-translate-y-[0px] group-hover:opacity-100"
         >
             <router-link
                 v-for="item in dropdownItems"
                 :key="item.text"
                 :to="item.link"
-                class="rounded-full border-2 border-transparent bg-primary px-4 py-2 text-white shadow-[20px] transition-all duration-200 hover:border-primary-light hover:bg-primary-dark hover:text-secondary-light"
+                class="text-nowrap rounded-full border-2 border-transparent bg-primary px-4 py-2 text-white shadow-[20px] transition-all duration-200 hover:border-primary-light hover:bg-primary-dark hover:text-secondary-light"
             >
                 {{ item.text }}
             </router-link>
