62f060d23bc82bd9543a771e572b89294f90b086
Jonathan Kjørlaug <jonathan@kjorlaug.no>
2025-10-27 20:13:46 +0100

    Started styling

--- src/components/general/NablaTable.vue
+++ src/components/general/NablaTable.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
     defineProps<{
-        showColumnTitles: boolean
+        showColumnTitles?: boolean
+        columnTitles?: string[]
     }>()
 </script>
 
 <template>
-    <table>
-        {{
-            test
-        }}
-    </table>
+    <div class="border overflow-hidden rounded-lg border-black">
+        <table class="border w-full table-auto">
+            <thead>
+                <tr v-for="colTitle in columnTitles" :key="colTitle">
+                    <th>
+                        {{ colTitle }}
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody
+                class="[&>tr:hover]:!bg-primary-25 [&>tr:nth-child(even)]:bg-neutralish [&>tr:nth-child(odd)]:bg-neutral [&>tr>td]:p-4"
+            >
+                <slot />
+            </tbody>
+        </table>
+    </div>
 </template>
