ee55420568fde0abfb3863af5b6b378bf8f1cd0f
Mikkel Vaaje-Kolstad <mikkelvk@hotmsil.com>
2026-04-23 19:38:31 +0200

    dimension of lang change flag are now both 11:8

--- src/components/main/header-links/FlagEn.vue
+++ src/components/main/header-links/FlagEn.vue
@@ -3,13 +3,13 @@
 <template>
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 60 30"
+        viewBox="0 0 22 16"
         class="inline-block"
     >
-        <path d="M0,0 v30 h60 v-30 z" fill="#012169" />
-        <path d="M0,0 L60,30 M60,0 L0,30" stroke="#fff" stroke-width="6" />
-        <path d="M0,0 L60,30 M60,0 L0,30" stroke="#C8102E" stroke-width="4" />
-        <path d="M30,0 v30 M0,15 h60" stroke="#fff" stroke-width="10" />
-        <path d="M30,0 v30 M0,15 h60" stroke="#C8102E" stroke-width="6" />
+        <path d="M0,0 v16 h22 v-16 z" fill="#012169" />
+        <path d="M0,0 L22,16 M22,0 L0,16" stroke="#fff" stroke-width="3" />
+        <path d="M0,0 L22,16 M22,0 L0,16" stroke="#C8102E" stroke-width="2" />
+        <path d="M11,0 v16 M0,8 h22" stroke="#fff" stroke-width="5" />
+        <path d="M11,0 v16 M0,8 h22" stroke="#C8102E" stroke-width="3" />
     </svg>
 </template>
