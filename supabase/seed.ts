4b5a6c22a5a2f3d6751bd121cc572e4114f7a9ab
Alexander Mitrokhin <alexamm@stud.ntnu.no>
2026-04-23 21:01:42 +0200

    fix db seeding

--- supabase/seed.ts
+++ supabase/seed.ts
@@ -15,7 +15,7 @@ type NablaGroupDict = { [id: string]: NablaGroup }
 // Need service key - this is standard for local instances
 const supabase = createClient<Database>(
     "http://127.0.0.1:54321",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU",
+    "sb_secret_N7UND0UgjKTVK-Uodkm0Hg_xSvEMPvz",
 )
 
 const faker = new Faker({
